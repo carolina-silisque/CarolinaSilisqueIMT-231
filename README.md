9d4998546c2b77281bd448ccd119174617a324c2
carolina-silisque <carolina.silisque@ucb.edu.bo>
2025-02-25 12:19:42 -0400

    practicas de clase

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Soy Carolina Silisque estudio Ingenieria Biomedica 
 este repositorio servira como una carpeta de notas para la materia 
 de Programacion Superior
+
---
+++ tareas 1 /eje1.sh
@@ -0,0 +1,15 @@
+#bin/bash
+#este comando muestra el directorio actual
+pwd /tareas 1
+#Acceder a la carpeta /tmp y listar su contenido (ls).
+cd /tmp
+ls
+#Volver al directorio personal (cd ~).
+cd ~
+#Crear una carpeta llamada practica_shell en el directorio personal (mkdir).
+mkdir practica_shell
+#Acceder a practica_shell (cd practica_shell).
+cd practica_shell
+#Mostrar el directorio actual (pwd).
+pwd  /practica_shell
+
